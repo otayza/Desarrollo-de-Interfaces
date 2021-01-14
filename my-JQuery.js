36138590f7e3b8d8455bd5652297fc88a1dc4415
Otayza <50828200+otayza@users.noreply.github.com>
2021-01-14 16:49:55 +0100

    Add files via upload

---
+++ my-JQuery.js
@@ -0,0 +1,36 @@
+
+function fadeIn(elemento,duracion,callback){
+    if(duracion==null||duracion==0){
+        elemento.style.opacity="1";
+    }else{
+        var opacidad=parseFloat(elemento.style.opacity);
+        var incremento=(1-opacidad)/duracion;
+        int1=setInterval(function(){
+            opacidad=opacidad+incremento;
+            elemento.style.opacity=opacidad;
+            if(opacidad>=1){
+                clearInterval(int1);
+            }
+        },1000);
+    }
+}
+
+function fadeOut(elemento,duracion,callback){
+    if(duracion==null||duracion==0){
+        elemento.style.opacity="0";
+    }else{
+        var opacidad=parseFloat(ele1.style.opacity);
+        var decremento=opacidad/duracion;
+        int2=setInterval(function(){
+            opacidad=opacidad-decremento;
+            elemento.style.opacity=opacidad;
+            if(opacidad<=0){
+                clearInterval(int2);
+            }
+        },1000);
+    }
+}
+
+function callback(){
+    
+}
