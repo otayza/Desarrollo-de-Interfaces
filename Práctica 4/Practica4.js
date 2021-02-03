c58766fda9c72b58b2bb1532bd6aa19a2a657ab5
Otayza <50828200+otayza@users.noreply.github.com>
2021-02-03 23:08:20 +0100

    Practica 4

---
+++ Práctica 4/Practica4.js
@@ -0,0 +1,86 @@
+var dificultad=["FÁCIL","DIFÍCIL","HARCORE"];
+
+function borrarTexto(){
+    var texto=document.getElementById("texto");
+    texto.innerHTML="";
+    var cuadro=crearCuadro();
+    texto.append(cuadro);
+    cuadro.innerHTML="Hey, soy Eduardo Otayza y tengo 21 años. Estudio en el IES Clara del Rey y espero sacarme este año mi título de técnico de Desarrollo de aplicaciones Web";
+}
+
+function crearCuadro(){
+    let ele=document.createElement("div");
+    ele.style.backgroundImage="url('./Imagenes/madera.jpg')";
+    ele.style.position="relative";
+    ele.style.top="5%";
+    ele.style.left="5%";
+    ele.style.width="90%";
+    ele.style.height="90%";
+    ele.style.fontSize="2.25rem";
+    ele.style.textAlign="center";
+    ele.style.paddingTop="50px";
+    ele.style.color="yellow";
+    return ele;
+}
+
+function seleccionarDificultad(){
+    var texto=document.getElementById("texto");
+    texto.innerHTML="";
+    var cuadro=crearCuadro();
+    texto.append(cuadro);
+    cuadro.style.display="flex";
+    cuadro.style.flexDirection="column";
+    cuadro.style.alignItems="center";
+    cuadro.style.justifyContent="space-around";
+    for(let i=0;i<3;i++){
+        cuadro.append(niveles(i));
+    }
+    
+}
+
+function niveles(numero){
+    let ele=document.createElement("button");
+    ele.style.width="75%";
+    ele.innerHTML=dificultad[numero];
+    ele.style.backgroundColor="yellow";
+    if(numero==2){
+        ele.style.color="red";
+    }
+    return ele;
+}
+
+function instrucciones(){
+    var texto=document.getElementById("texto");
+    texto.innerHTML="";
+    var cuadro=crearCuadro();
+    texto.append(cuadro);
+    cuadro.innerHTML="1. Se juega con RATÓN<br>2. Dispones de un tiempo limitado para disparar a los pollos, ¡consigue la cena para esta noche!<br>3.Cuantos más pollos, más comida";
+}
+
+class BotonJugar extends HTMLElement{
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        this.style.width="50%";
+        this.style.height="20%";
+        var ele=document.createElement("button");
+        ele.innerHTML="Jugar";
+        ele.id="jugar";
+        
+        
+
+        this.append(ele);
+        var obj=this;
+        setInterval(function(){
+            if(obj.childNodes[0].getAttribute("style")=="color:green"){
+                obj.childNodes[0].setAttribute("style","color:yellow");          
+            }else{
+                obj.childNodes[0].setAttribute("style","color:green");
+            }
+        },1000)
+    }
+}
+
+customElements.define('boton-jugar', BotonJugar);
